ab08a777f27f03bcd4c369c3b4290aab0ff3318e
Abhishek C <abhishek@Abhisheks-MacBook-Air.local>
2022-11-25 12:49:12 +0800

    added repo

--- kafka-consumer/src/main/java/springboot/net/java/repository/WikimediaDataRepo.java
+++ kafka-consumer/src/main/java/springboot/net/java/repository/WikimediaDataRepo.java
@@ -1,8 +1,10 @@
 package springboot.net.java.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import springboot.net.java.entity.Wikimedia;
 
+@Repository
 public interface WikimediaDataRepo extends JpaRepository<Wikimedia, Long>{
 
 }
